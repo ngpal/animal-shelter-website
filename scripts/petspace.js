3c1acd3608affc4d8c193495715ff7337c85f0a1
nandu <nandagopalnmenon@gmail.com>
2024-05-18 12:10:35 +0530

    dog and cat sliders added

--- scripts/petspace.js
+++ scripts/petspace.js
@@ -2,44 +2,64 @@ let data = null;
 fetch("../sampledata.json").then((response) => {
     response.json().then((sampledata) => {
         const data = sampledata;
-        let list = document.getElementById("productList");
-        data.dogs.forEach((dog) => {
-            let div = document.createElement("div");
-            div.className = "product-card";
-            let img = document.createElement("img");
-            img.src = dog.image;
-            img.alt = dog.name;
-            let h3 = document
-                .createElement("h3")
-                .appendChild(document.createTextNode(dog.name));
-            let p = document
-                .createElement("p")
-                .appendChild(document.createTextNode(dog.breed));
-
-            div.appendChild(img);
-            div.appendChild(h3);
-            div.appendChild(p);
-
-            list.appendChild(div);
-        });
+        populateDogList(data);
+        populateCatList(data);
     });
 });
 
+let populateDogList = function (data) {
+    let list = document.getElementById("dogList");
+    data.dogs.forEach((dog) => {
+        let div = document.createElement("div");
+        div.className = "product-card";
+        let img = document.createElement("img");
+        img.src = dog.image;
+        img.alt = dog.name;
+        let h3 = document.createElement("h3");
+        h3.innerHTML = dog.name;
+        let p = document.createElement("p");
+        p.innerHTML = dog.breed;
+        div.appendChild(img);
+        div.appendChild(h3);
+        div.appendChild(p);
+
+        list.appendChild(div);
+    });
+};
+
+let populateCatList = function (data) {
+    let list = document.getElementById("catList");
+    data.cats.forEach((cat) => {
+        let div = document.createElement("div");
+        div.className = "product-card";
+        let img = document.createElement("img");
+        img.src = cat.image;
+        img.alt = cat.name;
+        let h3 = document.createElement("h3");
+        h3.innerHTML = cat.name;
+        let p = document.createElement("p");
+        p.innerHTML = cat.breed;
+        div.appendChild(img);
+        div.appendChild(h3);
+        div.appendChild(p);
+
+        list.appendChild(div);
+    });
+};
+
 document.addEventListener("DOMContentLoaded", function () {
-    const scrollRight = document.getElementById("scrollRight");
-    const productListContainer = document.querySelector(
-        ".product-list-container"
-    );
+    const scrollRight = document.getElementsByClassName("scroll-icon");
 
-    console.log("Initial Scroll Left:", productListContainer.scrollLeft);
-
-    scrollRight.addEventListener("click", function () {
-        console.log("Clicked!");
-        productListContainer.scrollBy({
-            top: 0,
-            left: 620,
-            behavior: "smooth",
+    Array.from(scrollRight).forEach((scroller) => {
+        const productListContainer = scroller.parentElement.querySelector(
+            ".product-list-container"
+        );
+        scroller.addEventListener("click", function () {
+            productListContainer.scrollBy({
+                top: 0,
+                left: 620,
+                behavior: "smooth",
+            });
         });
-        console.log("Scroll Left After:", productListContainer.scrollLeft);
     });
 });
